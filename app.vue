b29049566f6a83111c93bbd18c5ca87cffbfeeca
Fuad Elhasan Irfani <73324192+feilvan@users.noreply.github.com>
2022-07-06 12:54:49 +0700

    rawr

--- app.vue
+++ app.vue
@@ -1,6 +1,29 @@
+<script setup>
+useHead({
+  title: 'rawr!',
+  link: [
+    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap' }
+  ]
+})
+</script>
+
 <template>
-  <div>
-    <!-- <NuxtWelcome /> -->
-    <span class=" text-8xl uppercase">wahallaw</span>
+  <div class="flex flex-1 flex-col p-4 w-full h-screen max-w-5xl my-0 mx-auto">
+    <NuxtLayout>
+      <NuxtPage/>
+      <footer>
+		    <span>©{{new Date().getFullYear()}} truclr</span>
+	    </footer>
+    </NuxtLayout>
   </div>
 </template>
+
+<style>
+:root {
+  @apply bg-black text-white;
+  font-family: 'JetBrains Mono', monospace;
+}
+footer {
+  @apply flex flex-col justify-center items-center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <img src="/images/dino.svg" alt="Chromium T-rex"/>
+    <span class=" font-bold mt-8">rraawwrr!</span>
+  </section>
+</template>
+
+<style>
+section {
+    @apply flex flex-col flex-1 justify-center items-center;
+}
+</style>
